d5b864bd6b50dd14057fc158021a47d86ba42706
leomartinez2020 <lowenhard@yahoo.com>
2022-11-08 00:23:16 +0000

    Add security

--- preicfes/settings.py
+++ preicfes/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$9s1#_ky38m)r%9_re8!b6ui5y@_!wd_)s+7piw#yw^fyq%&a)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['18.234.209.76', 'ec2-18-234-209-76.compute-1.amazonaws.com']
 
